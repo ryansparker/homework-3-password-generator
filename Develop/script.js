3df7510b5488742d7e3978380f634c94c0022dd0
Ryan Parker <ryansparker@gmail.com>
2020-03-12 16:04:34 -0400

    made vaiables and prompt windows

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//pick password length
+var passwordLength = prompt ("What is the character length of your password (between 8 & 128 only)")
+
+//confirming character types
+var confirmLowercase = confirm ("Do you want lowercase characters?");
+var confirmUppercase = confirm ("Do you want uppercase characters?");
+var confirmNumeric = confirm ("do you want numeric characters?");
+var confirmSpecial = confirm ("Do you want special characters?")
+
+//if user wants lowercase characters
+if (confirmLowercase) {
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
